6bcbc425662a273f64265034b51fcd75729b775e
BitaShamsafar <bita.shamsafar@hotmail.com>
2018-11-28 20:26:26 +0330

    Updates

---
+++ precache-manifest.c0083c397d39b18dbb675fd43beb95d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17c539d30a247d059619",
+    "url": "/React-SearchBox/static/js/runtime~main.17c539d3.js"
+  },
+  {
+    "revision": "d7e1aa50653850203abc",
+    "url": "/React-SearchBox/static/js/main.d7e1aa50.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/React-SearchBox/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "d7e1aa50653850203abc",
+    "url": "/React-SearchBox/static/css/main.2184fb99.chunk.css"
+  },
+  {
+    "revision": "111fe27a2c2ffca30ee1a364c16a2414",
+    "url": "/React-SearchBox/index.html"
+  }
+];
